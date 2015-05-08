cdb39c4aac492e99465cdf8fff4b01f8773e1e2a
Gabe Johnson <gabe@blankslatesystems.com>
2015-05-08 14:34:42 -0600

    add main

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a string = "initial"
+	fmt.Println(a)
+	var x, y int = 2, 6
+	fmt.Println("2 + 6 is", x+y)
+	fmt.Println("pi is roughly", y)
+}
